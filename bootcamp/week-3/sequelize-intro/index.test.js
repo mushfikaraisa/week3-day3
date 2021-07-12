7c8453a39288a6e0680dd488434b660728ab541d
danceoval <daniel.sohval@gmail.com>
2021-07-12 14:35:40 -0400

    adds sequelize associations

--- bootcamp/week-3/sequelize-intro/index.test.js
+++ bootcamp/week-3/sequelize-intro/index.test.js
@@ -1,6 +1,8 @@
 const {sequelize} = require("./db");
 
-const {Musician} = require("./index")
+const {Musician, Band} = require("./index") // pull them from index, where we make our association
+
+// const {Band} = require('./Band') //why is this bad? Because our Band doesn't have an association here!
 
 //Destructuring Syntax
 // const {banana} = require('./fruit')
@@ -18,10 +20,39 @@ describe('Musician Database', () => {
 		expect(testMusician.name).toBe('Prince')
 	})
 
-	test('can play an instrument', async () => {
+	test('musicians can play an instrument', async () => {
 		const testMusician = await Musician.create({name: 'Jimi Hendrix', instrument : "Guitar"});
-		console.log("TEST MUSICIAN: ", testMusician)
 		expect(testMusician.instrument).toBe('Guitar');
 	})
+
+	test('can create a band', async () => {
+		const testBand = await Band.create({name : 'TLC'})
+		expect(testBand.name).toBe('TLC')
+	})
+
+	test('bands have a genre', async() => {
+		const testBand = await Band.create({name : 'Spice Girls', genre : 'Pop'})
+		expect(testBand.genre).toBe('Pop')
+	})
+
+	test('Bands can have many musicians', async () => {
+		const BTS = await Band.create({name : 'BTS', genre : 'Pop'})
+
+		const jungkook = await Musician.create({name : 'Jungkook', instrument : 'vocals'});
+		const jimin = await Musician.create({name : 'Jimin', instrument : 'vocals' });
+		const suga = await Musician.create({name : 'Suga', instrument : 'rap'});
+
+		await BTS.addMusician(jungkook) //addMusician is a 'magic method' we get from Sequelize, once we declare an association
+		await BTS.addMusician(jimin)
+		await BTS.addMusician(suga)
+
+		const musicians = await BTS.getMusicians() // another association 'magic method'
+
+		expect(musicians.length).toBe(3)
+		expect(musicians[0] instanceof Musician).toBeTruthy
+
+	})
+
+	
 
 })
